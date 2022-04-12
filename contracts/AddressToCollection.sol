322a1f53864f279ec90b778417525ed66d80ea38
dbvora03 <54694188+dbvora03@users.noreply.github.com>
2022-04-12 11:01:00 -0600

    added facotry functionality

--- contracts/AddressToCollection.sol
+++ contracts/AddressToCollection.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "./MyNFT.sol";
+
 contract AddressToCollection {
 
   struct addressData {
@@ -8,21 +10,29 @@ contract AddressToCollection {
     address[] addresses;
   }
 
-  mapping(string => addressData) private addressMapping;
+  mapping(address => addressData) private addressMapping;
 
-  function getCollections(string memory userAddress) public view returns (address[] memory){
-    require(addressMapping[userAddress].exists);
-    return addressMapping[userAddress].addresses;
+  function getCollections() public view returns (address[] memory){
+    require(addressMapping[msg.sender].exists);
+    return addressMapping[msg.sender].addresses;
   }
 
-  function addCollection(string memory userAddress, address theAddress) public {
-    if (addressMapping[userAddress].exists) {
-      addressMapping[userAddress].addresses.push(theAddress);
+  function getSomeoneElsesCollection(address wallet_address) public view returns (address[] memory) {
+    require(addressMapping[wallet_address].exists);
+    return addressMapping[wallet_address].addresses;
+  }
+
+  function addCollection(string memory collection_name, string memory collection_symbol) public {
+
+    MyNFT collection = new MyNFT(collection_name, collection_symbol);
+
+    if (addressMapping[msg.sender].exists) {
+      addressMapping[msg.sender].addresses.push(address(collection));
     } else {
       address[] memory emptyAddressList;
       addressData memory _address = addressData(true, emptyAddressList);
-      addressMapping[userAddress] = _address;
-      addressMapping[userAddress].addresses.push(theAddress);
+      addressMapping[msg.sender] = _address;
+      addressMapping[msg.sender].addresses.push(address(collection));
     }
   }
 }
