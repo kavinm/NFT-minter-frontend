6348633c463024a0165e0e6078ad8de987060b0b
kavinm <30965883+kavinm@users.noreply.github.com>
2022-08-07 17:23:42 -0400

    sft script

--- test/sample-test.js
+++ test/sample-test.js
@@ -7,11 +7,17 @@ describe("SFT", function () {
     const sft = await SFT.deploy(
       "https://api.jsonbin.io/v3/qs/62ec2b9a5c146d63ca5f288f"
     );
-    await greeter.deployed();
+    await sft.deployed();
+    console.log("Contract deployed to:", sft.address);
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    let txn = await sft.mint(msg.sender);
+    console.log("Minted NFT 1");
+    await txn.wait();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    //expect(await sft.greet()).to.equal("Hello, world!");
+    expect(await sft.uri(1)).to.equal(
+      "https://api.jsonbin.io/v3/qs/62ec2b9a5c146d63ca5f288f"
+    );
 
     // wait until the transaction is mined
     await setGreetingTx.wait();
